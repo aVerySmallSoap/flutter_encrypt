f8c5424607b425ff54fa708bc846a6183ea8d05a
Lirys <iamyourmarkieboo@gmail.com>
2024-11-21 14:34:36 +0800

    Separated Class concerns and implemented an interface

--- lib/test.dart
+++ lib/test.dart
@@ -2,8 +2,12 @@ import 'package:test_app/api/database.dart';
 import 'package:test_app/api/login.dart';
 import 'package:test_app/api/registration.dart';
 
+import 'Cipher.dart';
+
 void main() async {
-  Database.init();
-  print(Login.login('tas', 'p'));
+  // Database.init();
+  // print(Login.login('tas', 'p'));
   // print(await Registration.register('tas', 'p'));
+  print(Cipher.encrypt.vigenere('cheese', 'key'));
+  print(Cipher.decrypt.vigenere('mlcowc', 'key'));
 }
