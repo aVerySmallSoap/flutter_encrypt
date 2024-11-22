e23de645e8d73789caaa939191768e6a1886b6a3
dongjilo <121302078+dongjilo@users.noreply.github.com>
2024-11-23 02:08:02 +0800

    Add new pages and text styles; update dependencies

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/textstyles/normal_text.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Text(
+          'User Settings',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add UI elements to edit user settings (username, password)
+            MyNormalText(text: 'User settings will be displayed here.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
