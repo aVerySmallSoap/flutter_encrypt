63e6cbf97f80995ca1df03ff9e401a713f6deaa3
Lirys <iamyourmarkieboo@gmail.com>
2024-11-29 12:20:16 +0800

    Fixed a bug where tapping does not lead to action @ cipherbuttons.dart

--- lib/components/cipherbuttons.dart
+++ lib/components/cipherbuttons.dart
@@ -19,89 +19,88 @@ class CipherButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: action,
-        child: Container(
-            height: 186,
-            width: double.maxFinite,
-            padding: EdgeInsets.all(16),
-            margin: EdgeInsets.only(top: 2),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.blue[400]!,
-                    Colors.blue[300]!,
-                    Colors.blue,
-                    Colors.lightBlue,
-                    Colors.lightBlue[400]!,
-                    Colors.lightBlue[300]!,
-                  ],
-                  transform: GradientRotation(180),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(0, 0, 0, 0.4),
-                    blurStyle: BlurStyle.outer,
-                    blurRadius: 8,
-                  ),
-                ],
+    return Container(
+      height: 186,
+      width: double.maxFinite,
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.only(top: 2),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue[400]!,
+              Colors.blue[300]!,
+              Colors.blue,
+              Colors.lightBlue,
+              Colors.lightBlue[400]!,
+              Colors.lightBlue[300]!,
+            ],
+            transform: GradientRotation(180),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.4),
+              blurStyle: BlurStyle.outer,
+              blurRadius: 8,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: ElevatedButton(
+          onPressed: action,
+          style: ButtonStyle(
+            shadowColor: WidgetStatePropertyAll(Colors.transparent),
+            backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16),
               ),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  shadowColor: WidgetStatePropertyAll(Colors.transparent),
-                  backgroundColor: WidgetStatePropertyAll(Colors.transparent),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      alignment: Alignment.centerRight,
+                      image: image,
                     ),
                   ),
                 ),
-                child: Stack(
+              ),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: 180,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Positioned(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            alignment: Alignment.centerRight,
-                            image: image,
-                          ),
-                        ),
+                    Text(
+                      text,
+                      style: GoogleFonts.openSans(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
                       ),
                     ),
-                    Container(
-                      constraints: BoxConstraints(
-                        maxWidth: 180,
+                    Text(
+                      desc,
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.openSans(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            text,
-                            style: GoogleFonts.openSans(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                          Text(
-                            desc,
-                            textAlign: TextAlign.left,
-                            style: GoogleFonts.openSans(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
+                    ),
                   ],
                 ),
-              ),
-            )));
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
