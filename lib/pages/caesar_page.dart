6eb20e85f3accf4988c64761661bb53ac92719b8
Lirys <iamyourmarkieboo@gmail.com>
2024-12-10 02:48:01 +0800

    Fixed an overflow bug on cipher pages

--- lib/pages/caesar_page.dart
+++ lib/pages/caesar_page.dart
@@ -31,175 +31,181 @@ class _CaesarPageState extends State<CaesarPage> {
           onPressed: () => Navigator.popAndPushNamed(context, "/home"),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 24),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(243, 180, 98, 1),
-                  Color.fromRGBO(240, 200, 127, 1),
-                  Color.fromRGBO(239, 219, 158, 1),
-                  Color.fromRGBO(242, 235, 192, 1),
-                ],
-                transform: GradientRotation(240),
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-            ),
-            margin: EdgeInsets.all(16),
-            height: 256,
-            child: Ink(
-              color: Colors.transparent,
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.white,
-                    width: double.maxFinite,
-                    padding: EdgeInsets.all(4),
-                    margin: EdgeInsets.only(top: 24, right: 8),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints:
+              BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 24),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(243, 180, 98, 1),
+                      Color.fromRGBO(240, 200, 127, 1),
+                      Color.fromRGBO(239, 219, 158, 1),
+                      Color.fromRGBO(242, 235, 192, 1),
+                    ],
+                    transform: GradientRotation(240),
                   ),
-                  Container(
-                    width: double.maxFinite,
-                    margin: EdgeInsets.all(8),
-                    padding: EdgeInsets.all(4),
-                    child: Text(
-                      "Caesar",
-                      style: TextStyle(
-                        fontFamily: "Antipasto",
-                        fontSize: 48,
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                ),
+                margin: EdgeInsets.all(16),
+                height: 256,
+                child: Ink(
+                  color: Colors.transparent,
+                  child: Column(
+                    children: [
+                      Container(
+                        color: Colors.white,
+                        width: double.maxFinite,
+                        padding: EdgeInsets.all(4),
+                        margin: EdgeInsets.only(top: 24, right: 8),
                       ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      _changeable,
-                      key: Key("caesar_key"),
-                      overflow: TextOverflow.fade,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: "Courier New",
-                        fontSize: 28,
+                      Container(
+                        width: double.maxFinite,
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(4),
+                        child: Text(
+                          "Caesar",
+                          style: TextStyle(
+                            fontFamily: "Antipasto",
+                            fontSize: 48,
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
                       ),
-                    ),
+                      Expanded(
+                        child: Text(
+                          _changeable,
+                          key: Key("caesar_key"),
+                          overflow: TextOverflow.fade,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Courier New",
+                            fontSize: 28,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.4),
-                      spreadRadius: 2,
-                      blurRadius: 8)
-                ],
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  topRight: Radius.circular(8),
                 ),
               ),
-              child: Container(
-                padding: EdgeInsets.only(top: 32, left: 32, right: 32),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(bottom: 18),
-                      child: TextField(
-                        controller: input,
-                        decoration: InputDecoration(
-                          labelText: "Word",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
-                        ),
-                      ),
+              Expanded(
+                child: Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.4),
+                          spreadRadius: 2,
+                          blurRadius: 8)
+                    ],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
                     ),
-                    Container(
-                      child: TextField(
-                        controller: shifts,
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                        ],
-                        decoration: InputDecoration(
-                          labelText: "Shifts",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 32, left: 32, right: 32),
+                    child: Column(
                       children: [
-                        Expanded(
-                          child: RadioListTile<Shift>(
-                            title: const Text("Left"),
-                            value: Shift.left,
-                            groupValue: direction,
-                            onChanged: (Shift? val) {
-                              setState(() {
-                                direction = val!;
-                              });
-                            },
+                        Container(
+                          padding: EdgeInsets.only(bottom: 18),
+                          child: TextField(
+                            controller: input,
+                            decoration: InputDecoration(
+                              labelText: "Word",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
+                            ),
                           ),
                         ),
-                        Expanded(
-                          child: RadioListTile<Shift>(
-                            title: const Text("Right"),
-                            value: Shift.right,
-                            groupValue: direction,
-                            onChanged: (Shift? val) {
-                              setState(() {
-                                direction = val!;
-                              });
+                        Container(
+                          child: TextField(
+                            controller: shifts,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
+                            decoration: InputDecoration(
+                              labelText: "Shifts",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: RadioListTile<Shift>(
+                                title: const Text("Left"),
+                                value: Shift.left,
+                                groupValue: direction,
+                                onChanged: (Shift? val) {
+                                  setState(() {
+                                    direction = val!;
+                                  });
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              child: RadioListTile<Shift>(
+                                title: const Text("Right"),
+                                value: Shift.right,
+                                groupValue: direction,
+                                onChanged: (Shift? val) {
+                                  setState(() {
+                                    direction = val!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: double.maxFinite,
+                          child: FilledButton(
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStatePropertyAll(
+                                Color.fromRGBO(243, 180, 98, 1),
+                              ),
+                            ),
+                            onPressed: () {
+                              Map<String, dynamic>? response =
+                                  Cipher.encrypt.caesar(
+                                direction,
+                                int.parse(shifts.text),
+                                input.text,
+                              );
+                              if (response?["status"] == STATUS.OK) {
+                                setState(() {
+                                  _changeable = response?["optional"];
+                                  SessionManager.instance
+                                      .getSession("user")
+                                      ?.user
+                                      ?.addHistory(response?["optional"]);
+                                });
+                              }
                             },
+                            child: Text("Encode"),
                           ),
                         ),
                       ],
                     ),
-                    Container(
-                      width: double.maxFinite,
-                      child: FilledButton(
-                        style: ButtonStyle(
-                          backgroundColor: WidgetStatePropertyAll(
-                            Color.fromRGBO(243, 180, 98, 1),
-                          ),
-                        ),
-                        onPressed: () {
-                          Map<String, dynamic>? response =
-                              Cipher.encrypt.caesar(
-                            direction,
-                            int.parse(shifts.text),
-                            input.text,
-                          );
-                          if (response?["status"] == STATUS.OK) {
-                            setState(() {
-                              _changeable = response?["optional"];
-                              SessionManager.instance
-                                  .getSession("user")
-                                  ?.user
-                                  ?.addHistory(response?["optional"]);
-                            });
-                          }
-                        },
-                        child: Text("Encode"),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/vigenere_page.dart
+++ lib/pages/vigenere_page.dart
@@ -27,140 +27,147 @@ class _VigenerePageState extends State<VigenerePage> {
           onPressed: () => Navigator.popAndPushNamed(context, "/home"),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 24),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(141, 191, 179, 1),
-                  Color.fromRGBO(158, 181, 149, 1),
-                  Color.fromRGBO(179, 168, 136, 1),
-                  Color.fromRGBO(191, 141, 152, 1),
-                ],
-                transform: GradientRotation(240),
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-            ),
-            margin: EdgeInsets.all(16),
-            height: 256,
-            child: Ink(
-              color: Colors.transparent,
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.white,
-                    width: double.maxFinite,
-                    padding: EdgeInsets.all(4),
-                    margin: EdgeInsets.only(top: 24, right: 8),
-                  ),
-                  Container(
-                    width: double.maxFinite,
-                    margin: EdgeInsets.all(8),
-                    padding: EdgeInsets.all(4),
-                    child: Text(
-                      "Vigenere",
-                      style: TextStyle(
-                        fontFamily: "Antipasto",
-                        fontSize: 48,
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      _changeable,
-                      key: Key("vigenere_key"),
-                      overflow: TextOverflow.fade,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: "Courier New",
-                        fontSize: 28,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
           ),
-          Expanded(
-            child: Container(
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.4),
-                      spreadRadius: 2,
-                      blurRadius: 8)
-                ],
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  topRight: Radius.circular(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 24),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(141, 191, 179, 1),
+                      Color.fromRGBO(158, 181, 149, 1),
+                      Color.fromRGBO(179, 168, 136, 1),
+                      Color.fromRGBO(191, 141, 152, 1),
+                    ],
+                    transform: GradientRotation(240),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                ),
+                margin: EdgeInsets.all(16),
+                height: 256,
+                child: Ink(
+                  color: Colors.transparent,
+                  child: Column(
+                    children: [
+                      Container(
+                        color: Colors.white,
+                        width: double.maxFinite,
+                        padding: EdgeInsets.all(4),
+                        margin: EdgeInsets.only(top: 24, right: 8),
+                      ),
+                      Container(
+                        width: double.maxFinite,
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(4),
+                        child: Text(
+                          "Vigenere",
+                          style: TextStyle(
+                            fontFamily: "Antipasto",
+                            fontSize: 48,
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          _changeable,
+                          key: Key("vigenere_key"),
+                          overflow: TextOverflow.fade,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Courier New",
+                            fontSize: 28,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              child: Container(
-                padding: EdgeInsets.only(top: 32, left: 32, right: 32),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(bottom: 18),
-                      child: TextField(
-                        controller: input,
-                        decoration: InputDecoration(
-                          labelText: "Word",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
+              Expanded(
+                child: Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.4),
+                          spreadRadius: 2,
+                          blurRadius: 8)
+                    ],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                    ),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 32, left: 32, right: 32),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(bottom: 18),
+                          child: TextField(
+                            controller: input,
+                            decoration: InputDecoration(
+                              labelText: "Word",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(bottom: 18),
-                      child: TextField(
-                        controller: _key,
-                        decoration: InputDecoration(
-                          labelText: "Key",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 18),
+                          child: TextField(
+                            controller: _key,
+                            decoration: InputDecoration(
+                              labelText: "Key",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Container(
-                      width: double.maxFinite,
-                      child: FilledButton(
-                        style: ButtonStyle(
-                          backgroundColor: WidgetStatePropertyAll(
-                            Color.fromRGBO(141, 191, 179, 1),
+                        Container(
+                          width: double.maxFinite,
+                          child: FilledButton(
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStatePropertyAll(
+                                Color.fromRGBO(141, 191, 179, 1),
+                              ),
+                            ),
+                            onPressed: () {
+                              Map<String, dynamic>? response = Cipher.encrypt
+                                  .vigenere(input.text, _key.text);
+                              if (response?["status"] == STATUS.OK) {
+                                setState(() {
+                                  _changeable = response?["optional"];
+                                  SessionManager.instance
+                                      .getSession("user")
+                                      ?.user
+                                      ?.addHistory(response?["optional"]);
+                                });
+                              }
+                            },
+                            child: Text("Encode"),
                           ),
                         ),
-                        onPressed: () {
-                          Map<String, dynamic>? response =
-                              Cipher.encrypt.vigenere(input.text, _key.text);
-                          if (response?["status"] == STATUS.OK) {
-                            setState(() {
-                              _changeable = response?["optional"];
-                              SessionManager.instance
-                                  .getSession("user")
-                                  ?.user
-                                  ?.addHistory(response?["optional"]);
-                            });
-                          }
-                        },
-                        child: Text("Encode"),
-                      ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
